2174dfc4d99e839d942c4a224a6b2d742e4d5c52
Michael Toth <michael@virtualpianist.com>
2020-07-28 16:31:35 -0400

    first

---
+++ Maze.py
@@ -0,0 +1,14 @@
+
+
+import turtle
+class Maze():
+    """ This class creates a random maze """
+
+    def __init__(self):
+        self.s = turtle.getscreen()
+        self.turtle = turtle.Turtle()
+        # YOUR CODE GOES HERE
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ MazeTests.py
@@ -0,0 +1,29 @@
+import unittest
+import turtle
+
+
+class MazeTests(unittest.TestCase):
+    def setUp(self):
+        self.m = Maze()
+
+    def testScreen(self):
+        self.assertTrue(type(self.m.s) == turtle._Screen, "No Screen!")
+
+    def testTurtle(self):
+        self.assertTrue(type(self.m.turtle) == turtle.Turtle)
+
+    def testBackground(self):
+        self.assertTrue(self.m.s.bgcolor() == 'blue')
+
+
+class Maze():
+    """ This class creates a random maze """
+
+    def __init__(self):
+        self.s = turtle.getscreen()
+        self.turtle = turtle.Turtle()
+        self.s.bgcolor("blue")
+
+
+if __name__ == "__main__":
+    unittest.main()
