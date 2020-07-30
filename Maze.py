06bbb8346a392cd8251e1f2901a7c8f475867c93
Michael Toth <michael@virtualpianist.com>
2020-07-30 01:10:45 -0400

    properly checks window size

--- Maze.py
+++ Maze.py
@@ -1,14 +1,14 @@
-
-
 import turtle
+
+
 class Maze():
     """ This class creates a random maze """
 
     def __init__(self):
-        self.s = turtle.getscreen()
+        self.s = turtle.Screen()
         self.turtle = turtle.Turtle()
-        # YOUR CODE GOES HERE
+        scr = turtle.Screen()
+        self.s.bgcolor("blue")
+        self.s.setup(width=400,height=400)
+        self.matrix = [[1 for i in range(20)] for j in range(20)]
 
-
-if __name__ == "__main__":
-    unittest.main()
--- MazeTests.py
+++ MazeTests.py
@@ -1,28 +1,22 @@
 import unittest
 import turtle
-
+from Maze import Maze
+SIZE = 400
 
 class MazeTests(unittest.TestCase):
     def setUp(self):
         self.m = Maze()
-
     def testScreen(self):
         self.assertTrue(type(self.m.s) == turtle._Screen, "No Screen!")
-
     def testTurtle(self):
         self.assertTrue(type(self.m.turtle) == turtle.Turtle)
-
     def testBackground(self):
         self.assertTrue(self.m.s.bgcolor() == 'blue')
-
-
-class Maze():
-    """ This class creates a random maze """
-
-    def __init__(self):
-        self.s = turtle.getscreen()
-        self.turtle = turtle.Turtle()
-        self.s.bgcolor("blue")
+    def testSize(self):
+        self.assertTrue(self.m.s.window_height() == SIZE, f"window height is {self.m.s.window_height()}")
+        self.assertTrue(self.m.s.window_width() == SIZE)
+    def testMatrixSize(self):
+        self.assertTrue(len(self.m.matrix)==SIZE/20)
 
 
 if __name__ == "__main__":
